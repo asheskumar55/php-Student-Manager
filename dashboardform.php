1e4bec955081e436b5ea53f9de995159e1e4dfe6
Ashes <asheskumar55@gmail.com>
2018-03-11 17:23:41 +0545

    Initial commit

---
+++ dashboardform.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/dashboardform.css">
+	<!--Offline Mode 
+	<link rel="stylesheet" href="D:\Bootstrap\bootstrap-4.0.0-dist_2\css\bootstrap.min.css">
+	<link rel="stylesheet" href="D:\Bootstrap\bootstrap-4.0.0-dist_2\js\bootstrap.min.js">
+	<link rel="stylesheet" href="D:\Bootstrap\bootstrap-4.0.0-dist_2\js\jquery.min.js">        
+	<!-  -Online Mode-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<title>Dashboard Form</title>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row content ashes">
+			<div class="col-sm-3 sidenav">
+				<h3 chass=Ashes>Student Manager</h3>
+				<ul class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#section1">Dashboard</a></li>
+					
+					<li> <a href="#section2">New Student</a></li>
+					<li><a href="#section3">Manage Branches</a></li>
+					<li><a href="#section4">Edit Profile </a></li>
+					<li><a href="#section5">Manage Users</a></li>
+					<li><a href="#section6">Manage Reports</a></li>
+					<li><a href="#section7">System Setup </a></li>
+					<li><a href="#section8">Help and Support </a></li>
+					
+				</ul><br>
+			</div>
+			<div class="col-sm-9 rightnav">
+				<h4><SMALL>Ashes Adhikari</SMALL></h4>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Student Manager</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/global.css">
+	<!--Offline Mode -->
+	<link rel="stylesheet" href="D:\Bootstrap\bootstrap-4.0.0-dist_2\css\bootstrap.min.css">
+	<link rel="stylesheet" href="D:\Bootstrap\bootstrap-4.0.0-dist_2\js\bootstrap.min.js">
+	<link rel="stylesheet" href="D:\Bootstrap\bootstrap-4.0.0-dist_2\js\jquery.min.js">
+	<!-- Online Mode -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid bg">
+	<div class="row">
+		<div class="col-md-4 col-sm-4 col-xm-12"></div>
+		<div class="col-md-4 col-sm-4 col-xm-12">
+		<!-- Form Start -->
+		<form class="form-container">
+			<h2 >Stundent Manager</h2>
+			<div class="form-group">
+				<label for="exampleInputEmail1" > Username: </label>
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Username">
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPassword1" >Password:</label>
+				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
+			</div>
+			<div class="checkbox"> 
+				<label>
+					<input type="checkbox"> Remember me
+				</label>
+			</div>
+			<button type="submit" class="btn btn-success btn-block">Login</button>
+
+		</form>
+		<!-- Form End -->
+			</div>
+		<div class="col-md-4 col-sm-4 col-xm-12"></div>	
+</body>
+</html>
